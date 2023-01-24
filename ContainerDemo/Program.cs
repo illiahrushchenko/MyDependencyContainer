f807ef2bb0bf7318f06bc28e544af6b5715617e1
Ilya Grush <gr.illya13@gmail.com>
2023-01-24 17:31:33 +0200

    Add project files.

---
+++ ContainerDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using MyDependecyContainer;
+using System;
+
+namespace ContainerDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ioc = new MyIoc();
+
+            ioc.Register<ProfileService>();
+            ioc.Register<EmailService>();
+
+
+            var emailService = ioc.Resolve<EmailService>();
+            emailService.Send();
+        }
+    }
+
+    class ProfileService
+    {
+        public string GetDefaultUser()
+        {
+            return "William Shakespear";
+        }
+    }
+
+    class EmailService
+    {
+        private readonly ProfileService _profileService;
+
+        public EmailService(ProfileService profileService)
+        {
+            _profileService = profileService;
+        }
+
+        public void Send()
+        {
+            Console.WriteLine($"Message sent to user {_profileService.GetDefaultUser()}");
+        }
+    }
+}
---
+++ MyDependecyContainer/MyIoc.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyDependecyContainer
+{
+    public class MyIoc
+    {
+        private readonly List<Type> _registeredTypes = new List<Type>();
+
+
+        public void Register<T>()
+        {
+            if (IsRegistered(typeof(T))){
+                throw new Exception($"Type {typeof(T)} is already registered.");
+            }
+            _registeredTypes.Add(typeof(T));
+        }
+
+        public T Resolve<T>()
+        {
+            if (!IsRegistered(typeof(T)))
+            {
+                throw new Exception($"Type {typeof(T)} is not registered.");
+            }
+            var parameters = GetParameters(typeof(T));
+
+            return (T)Activator.CreateInstance(typeof(T), parameters);
+        }
+
+        private object[] GetParameters(Type type)
+        {
+            var constructorInfo = type.GetConstructors().Single();
+            var parameters = constructorInfo.GetParameters();
+
+            var result = new object[parameters.Length];
+
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                result[i] = Activator.CreateInstance(parameters[i].ParameterType,
+                    GetParameters(parameters[i].ParameterType));
+            }
+
+            return result;
+        }
+
+        private bool IsRegistered(Type type)
+        {
+            return _registeredTypes.Contains(type);
+        }
+    }
+}
